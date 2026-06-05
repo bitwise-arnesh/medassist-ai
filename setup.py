b16e8df6f5ab963af4077f0c4031bbf54c41b997
Arnesh Bera <arnesh2018@gmail.com>
2026-06-05 16:49:51 +0530

    Initial project setup and dependency configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medassist_ai",
+    version="0.1.0",
+    author="Arnesh Bera",
+    packages=find_packages(),
+    install_requires=[]
+)
